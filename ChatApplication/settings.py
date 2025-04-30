244b6118fb122b99661ca051588215452b07274a
tejaslakade99 <tejaslakade99@gmail.com>
2025-04-30 18:49:11 +0530

    fix/added nixpacks.toml

--- ChatApplication/settings.py
+++ ChatApplication/settings.py
@@ -131,13 +131,13 @@ if ENVIRONMENT == 'development':
 if ENVIRONMENT == 'development':
     DATABASES = {
         'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST'),
-        'PORT': os.getenv('POSTGRES_PORT'),
-    }
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('POSTGRES_DB'),
+            'USER': os.getenv('POSTGRES_USER'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+            'HOST': os.getenv('POSTGRES_HOST'),
+            'PORT': os.getenv('POSTGRES_PORT'),
+        }
     }
 else:
     DATABASES = {
